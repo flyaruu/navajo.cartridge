a3c458e417c062847cc153c5520da2705dc3d575
Frank Lyaruu <flyaruu@gmail.com>
2013-07-18 14:54:11 +0200

    update

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/bin/sh 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+getsrc() {
+	echo $1
+	echo $2
+    ( cd "$2" && curl -O "$1" )
+}
+
+source $DIR/variables.sh
+downloadKaraf() {
+	if [ ! -d ${DOWNLOAD} ]; then
+		mkdir -p ${DOWNLOAD}
+	fi
+	if [ ! -f ${DOWNLOAD}/${KARAF_FILE} ]; then
+		echo "needs download"
+		getsrc ${KARAF_URL}${KARAF_FILE} ${DOWNLOAD}
+	fi
+}
+
+installKaraf() {
+	rm -rf $RUNTIME
+	mkdir -p $RUNTIME
+	cd $RUNTIME
+	echo "Runtime: ${RUNTIME}"
+	cp $DOWNLOAD/$KARAF_FILE .
+	echo "KarafFile: ${DOWNLOAD}/${KARAF_FILE}"
+	tar xfz $KARAF_FILE
+	rm $KARAF_FILE
+	mv $KARAF_VERSION karaf
+	cd karaf
+	cp -r ${KARAF_CONFIG}/* .
+	cp ${DIR}/*.sh bin/ 
+	cp ${DIR}/bin/setup ${DIR}/bin/control bin/ 
+	cp -r ${DIR}/metadata $RUNTIME/
+	cp -r ${DIR}/env ${DIR}/hooks $RUNTIME/
+	cd $RUNTIME
+	mv karaf/* $RUNTIME
+	rmdir $RUNTIME/karaf
+}
+
+appendTemplate() {
+	cd $RUNTIME
+	git clone https://github.com/Dexels/navajo.template.git template.git --bare
+}
+ 
+pushDeployedRepo() {
+	cd $RUNTIME
+	git init
+	git add *
+	git commit -a -m "initial"
+	git remote add origin git@github.com:Dexels/navajo.karaf.cartridge.git
+	git push origin master -f
+}
+
+
+downloadKaraf
+installKaraf
+appendTemplate
+pushDeployedRepo
---
+++ variables.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE=${DIR}
+TMP=$BASE/tmp
+RUNTIME=$BASE/target
+TIPIDIR=$BASE/../../tipi
+DOWNLOAD=$TMP/download
+KARAF_VERSION=apache-karaf-3.0.0.RC1
+KARAF_FILE=${KARAF_VERSION}.tar.gz
+KARAF_CONFIG=${DIR}/${KARAF_VERSION}.config
+KARAF_URL=http://ftp.nluug.nl/internet/apache/karaf/3.0.0.RC1/
+NAVAJO_OPTS="-DtipiDir=${TIPIDIR}"
