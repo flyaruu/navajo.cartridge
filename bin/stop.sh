a5aa19d75156191c82091dacb4b04b75c30dd130
Frank Lyaruu <flyaruu@gmail.com>
2014-02-18 10:23:56 +0100

    fix cartridge

--- bin/stop.sh
+++ bin/stop.sh
@@ -3,8 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "sourcing: ${DIR}/variables.sh"
 source ${DIR}/variables.sh
 
-cd ${RUNTIME}/karaf/bin
 set +e
+cd ${RUNTIME}/karaf/bin
 ./stop
 
 
