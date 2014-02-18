a919f99b14c9b66213783dd64b41ac4f51f53aac
Frank Lyaruu <flyaruu@gmail.com>
2014-02-18 10:34:14 +0100

    fix cartridge

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-source ./variables.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source ${DIR}/variables.sh
 echo "Cleaning folder: ${RUNTIME}"
 rm -rf ${RUNTIME}
 echo "Cleaning temp: ${TMP}"
