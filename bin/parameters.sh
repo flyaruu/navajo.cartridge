8e86962f812a569cb9dbe08e18d041dc3dad117a
Frank Lyaruu <flyaruu@gmail.com>
2014-02-18 12:08:58 +0100

    fix cartridge

--- bin/parameters.sh
+++ bin/parameters.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+set -e
+set -z
+
+if [ -z ${OPENSHIFT_APP_NAME} ]; then
+  echo "OPENSHIFT DETECTED:"
+  set
+
+else
+fi
+
+
 export RMI_REGISTRY_PORT=31099
 export RMI_SERVER_PORT=34444
 export KARAF_SSH=18101
-export HTTP_PORT=7171
+export HTTP_PORT=8080
 
